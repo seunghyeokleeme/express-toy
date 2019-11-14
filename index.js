9a13c0b8d96faaf431e496b60cb154ebd632ba25
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-11-15 04:55:29 +0900

    Express Server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("express-toy");
+const express = require("express");
+const app = express();
+
+const PORT = 4001;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.get("/", handleHome);
+
+app.listen(PORT, handleListening);
